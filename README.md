b9f7b2a589a63947c3b27a8615f562aebe5e6572
Leena <73400638+Leena476@users.noreply.github.com>
2020-11-08 14:09:26 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Project-22
-SUPPLY MISSION - 1: Drop Supplies And Medical Kits In The Middle Of A Zombie Apocalypse
+# SupplyMission
---
+++ sketch.js
@@ -0,0 +1,86 @@
+//define global variables - helicopter, package, ground
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+
+//define constants - engine, world, bodies
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload() {
+	
+	//load helicopter image
+	helicopterIMG = loadImage("helicopter.png");
+	
+	//load package image
+	packageIMG = loadImage("package.png");
+
+}
+
+function setup() {
+	
+	//create the canvas
+	createCanvas(800, 700);
+	
+	//change the rect mode to center
+	rectMode(CENTER);
+	
+	//define, scale, and add an image to the package sprite
+	packageSprite = createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale = 0.15;
+
+	//define, scale, and add an image to the helicopter sprite
+	helicopterSprite = createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale = 0.6;
+
+	//define and color the ground sprite
+	groundSprite = createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor = color(255);
+
+	//essential lines to create angine and add the world to the engine
+	engine = Engine.create();
+	world = engine.world;
+
+	//define package body and add it to the world
+	packageBody = Bodies.circle(width/2,200,5,{restitution : 0.5,isStatic : true});
+	World.add(world,packageBody);
+	
+	//define ground and add it to the world
+	ground = Bodies.rectangle(width/2,650,width,10,{isStatic : true});
+ 	World.add(world,ground);
+
+	//update "Engine" with the "engine"
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  
+  //change rect mode to center
+  rectMode(CENTER);
+  
+  //set background color to black (zero)
+  background(0);
+  
+  //make position of package sprite to match the package body
+  packageSprite.x = packageBody.position.x;
+  packageSprite.y = packageBody.position.y;
+  
+  //call keyPressed() function
+  keyPressed();
+  
+  drawSprites();
+
+}
+
+function keyPressed() {
+ 
+	//if the down arrow is pressed, the static of the package body becomes false
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(packageBody, false);
+	}
+	  
+}
